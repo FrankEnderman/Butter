6cedcb922693f350c9ba7e682af77cd341ddd327
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-09-29 14:30:31 +0530

    BETTER COMPATIBILITY FOR NT!

---
+++ Trinity NT Edition.cmd
@@ -0,0 +1,502 @@
+ @echo off
+color 7
+title Butter Trinity Winter 1.5 Beta 2
+echo BootSuccessful.%random%.%time%(%date%) > Boot.INF
+goto boot
+:boot
+cls
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@QX)E@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@E)XQ@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@E^`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `^E@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@G=      `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@w`      =W@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@M!          L@@@@@@@@@@@@@@@@@@@@@@@@@@L          !M@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@Q*             r#@@@@@@@@@@@@@@@@@@@@@@#r             *Q@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@l`               _0@@@@@@@@@@@@@@@@@@@@0_               `u@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@#r                  .M@@@@@@@@@@@@@@@@@@M.                  r#@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@#^                    `j@@@@@@@@@@@@@@@@j`                    ^#@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@)                       }@@@@@@@@@@@@@@}                       )@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@k                         r#@@@@@@@@@@#r                         k@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@Q-                         `j@@@@@@@@@@j`                         -Q@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@T                        `}B@@@Qd3KdQ@@@B}`                        l@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@#:                       _D@@#V:      :V#@@E_                       :#@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@g`                      `d@@Q:          :Q@@d`                      `g@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@d                       ~@@@v            v@@@~                       d@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@v            v@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Q:          :Q@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#y:      :V#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Qd33dQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0I0#@@@@#0I0@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `-,,-`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@k`            `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@L                L@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#r                  r#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B=                    =B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@g,                      ,g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@I`                        `X@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@}                            }@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#)                              )#@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@#r                                r#@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#du^-                        -^uO#@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@QZk]*!_'`      `'_!*]kZQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BQQQQB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 3 /NOBREAK >nul
+echo welcome!
+timeout /T 1 /NOBREAK >nul
+echo finding kernel.......
+timeout /T 1 /NOBREAK >nul
+echo Booting Butter Trinity ... 
+timeout /T 1 /NOBREAK >nul
+echo Loading.........
+timeout /T 1 /NOBREAK >nul
+goto login
+:login
+echo 01011 > LOGON.ind
+echo enter your account name
+cls 
+set/p name= enter name:
+if %name% == null set %name% == %username%
+echo.
+set country= type country code
+echo.
+echo Hello! %name% it is %time% now.
+echo.
+pause
+:menu
+cls
+echo What Would You Like To Do? Type the number.
+echo.
+echo 1.SETUP (FOR NEW USERS!) 2.useful tools menu 3.dates menu 4.change text color to green 5.TimeSetter 6.browser menu  7.accessories
+echo.
+ECHO current time: %time% date: %date% name: %name% 
+set /p input= choose:
+if %input% == 1 goto butterDeploy
+if %input% == 2 goto menu2
+if %input% == 3 goto datesMenu
+if %input% == 4 goto textColor
+if %input% == 5 goto timeSetter
+if %input% == 6 goto browserMenu
+if %input% == 7 goto accessories
+:accessories
+cls
+echo 1.change text color 2.controls 3.System Info 4.useful tools 5.games
+echo.
+set FHSK= option:
+if %FHSK% == 1 goto textColor
+if %FHSK% == 2 goto controls
+if %FHSK% == 3 goto info
+if %FHSK% == 4 goto menu2
+if %FHSK% == 5 goto gamesMenu
+:gamesMenu
+cls
+echo Dragon Killa
+pause
+cls
+echo The dragon attacked you!
+pause
+echo press 1 to attack the dragon. press 2 to surrender
+set P= press:
+if %P% == 1 goto win
+:win
+cls
+pause
+goto menu
+:info
+echo.===========================
+timeout /T 1 /NOBREAK >nul
+echo     Butter Trinity OS
+timeout /T 1 /NOBREAK >nul
+echo.===========================
+timeout /T 1 /NOBREAK >nul
+echo     DETAILS  
+timeout /T 1 /NOBREAK >nul
+echo.
+timeout /T 1 /NOBREAK >nul
+echo     VERSION = Winter 1.5 B2
+timeout /T 1 /NOBREAK >nul
+echo.     
+timeout /T 1 /NOBREAK >nul
+echo     RAM = 1GB
+timeout /T 1 /NOBREAK >nul
+echo.
+echo     CORE = %random%
+echo.
+timeout /T 1 /NOBREAK >nul
+echo     HARD_DRIVE = 250GB     
+echo.   
+timeout /T 1 /NOBREAK >nul
+echo     Kernel ver = 5.8.4
+timeout /T 1 /NOBREAK >nul
+echo.  
+echo    Build = 3
+echo.
+timeout /T 1 /NOBREAK >nul
+echo.==========================
+timeout /T 2 /NOBREAK >nul
+echo  @Copyright Interstar.inc 
+timeout /T 2 /NOBREAK >nul
+echo.
+echo  GPL Public Liscense 3.0
+timeout /T 2 /NOBREAK >nul
+echo.=========================
+timeout /T 2 /NOBREAK >nul
+echo     account
+timeout /T 1 /NOBREAK >nul
+echo.
+echo  name: %name%
+timeout /T 1 /NOBREAK >nul
+echo.
+timeout /T 1 /NOBREAK >nul
+echo  time: %time%
+timeout /T 1 /NOBREAK >nul
+echo.
+echo  date: %date%
+pause
+goto menu
+:clock 
+cls
+ECHO %time%
+pause 
+goto menu
+:cal
+cls 
+echo %date%
+echo enter 1 to return to menu
+set /p input=
+if %input% == 1 goto menu
+:calc
+title Calculator- butter OS
+cls
+title CALCULATOR- butter OS
+ECHO Calculator Version 1.4
+echo.
+ECHO * = MULTIPLY
+ECHO + = ADD
+ECHO - = SUBTRACT
+ECHO / = DIVIDE
+echo % = boolean/remainder
+SET /p UDefine= 
+SET /a UDefine=%UDefine%
+ECHO =
+ECHO %UDefine%
+ECHO.
+pause
+goto menu
+:TEXT
+cls
+echo TEXT EDITOR 1.1
+echo Enter name of the text file you want to make
+echo. 
+set /p N=Name 
+cls
+set /p WRITE=Write Your Document 
+echo %WRITE% > %N%.txt
+goto HOME
+:matrix
+cls
+echo %random%
+goto matrix
+:Help
+cls
+echo Text editor is a program made for creating text documents
+echo Calculator lets you process math problems
+echo BrokenMode is for breaking your computer (DANGEROUS)
+echo Matrix creates random numbers forever (eg. %random%)
+pause
+goto menu
+:powerOptions
+cls
+echo 1.reboot 2.boot into windows 3.return to menu 
+set /p input=
+if %input% == 1 goto boot
+if %input% == 2 exit
+if %input% == 3 goto menu
+:productivity
+echo 1.notes 2.calculator 3.menu 4.browse THE WEB!!
+set /p input= select:
+if %input% == 1 goto TEXT
+if %input% == 2 goto calc
+if %input% == 3 goto menu
+if %input% == 4 goto browse
+:settings
+cls
+echo 1.return to menu 
+echo set the time:
+set /p OPT= new time
+set %time% == %OPT%
+if %OPT% == 1 goto menu
+:spinningRage
+start "C:\Users\%username%\OneDrive\Desktop\Flipp Flapp v1.2.bat"
+:random
+cls 
+echo %random%%random%%random%%random%%random%%random%%random%
+pause
+goto menu
+:datesMenu
+cls
+echo 1.calendar 2.time 3.menu
+set /p direct= select:
+if %direct% == 1 goto cal
+if %direct% == 2 goto clock
+if %direct% == 3 goto menu
+:browser
+cls
+start iexplore
+pause
+exit
+:searchHistoryFinder
+for /f "delims=: tokens=2" %%i in ('ipconfig /displaydns^|find "Record Name"') do (find "%%i" /i history.txt >nul 2>&1|| echo %%i >>history.txt echo %%i)
+timeout /nobreak 5 >nul 2>&1
+sort history.txt /o history.txt
+goto searchHistoryFinder
+pause
+exit
+:browserMenu
+echo 1.start browser 2.view search history 3.menu
+set /p SD= sel:
+if %SD% == 1 goto browser
+if %SD% == 2 goto searchHistoryFinder
+if %SD% == 3 goto menu
+:menu2
+echo 1.calc 2.text editor 3.ASCII paintings 4.help 5.Drive List 6.unwrap the Winter Gift 7.story 8.personalize
+set /p OPTI= select option:
+if %OPTI% == 1 goto calc
+if %OPTI% == 2 goto UAC
+if %OPTI% == 3 goto ASCIIPaintings
+if %OPTI% == 4 goto help
+if %OPTI% == 5 goto DVDList
+if %OPTI% == 6 goto winterGift
+if %OPTI% == 7 goto story
+if %input% == 8 goto textColor
+:winterGift
+cls
+echo here is the changes:
+echo 1.changed the text color
+echo 2.made some cool changes
+pause
+goto menu2
+:UAC
+cls
+echo do you want to let this program to change your kernel?
+set /p FD= Yes = 1 No = 2:
+if %FD% == 1 goto TEXT
+if %FD% == 2 goto menu
+
+:ctrlPanel
+cls
+echo 1.power options 2.productivity 3.menu 4.BROKEN MODE (DANGER)
+echo 5.show nuclear show :D 6.NUKE 7.Hi
+set /p DFF= select:
+if %DFF% == 1 goto powerOptions
+if %DFF% == 2 goto menu2
+if %DFF% == 3 goto menu
+if %DFF% == 4 goto brokenMode
+if %DFF% == 5 goto nuclearIcon
+if %DFF% == 6 goto nuke
+if %DFF% == 7 goto hi
+:nuke
+cls
+echo 1.NUCLEAR ANNIHILATION 2.surrender
+set IO= press:
+if %IO% == 1 annihilated
+if %IO% == 2 surrender
+:annihilated
+cls 
+echo you kinda lost (deaths: %random%%random%%random%)
+pause
+goto menu
+:surrender
+cls
+echo at least you survived
+pause
+goto menu
+:nuclearIcon
+cls
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@QX)E@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@E)XQ@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@E^`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `^E@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@G=      `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@w`      =W@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@M!          L@@@@@@@@@@@@@@@@@@@@@@@@@@L          !M@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@Q*             r#@@@@@@@@@@@@@@@@@@@@@@#r             *Q@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@l`               _0@@@@@@@@@@@@@@@@@@@@0_               `u@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@#r                  .M@@@@@@@@@@@@@@@@@@M.                  r#@@@@@@@@@@@
+echo @@@@@@@@@@#^                    `j@@@@@@@@@@@@@@@@j`                    ^#@@@@@@@@@@
+echo @@@@@@@@@@)                       }@@@@@@@@@@@@@@}                       )@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@k                         r#@@@@@@@@@@#r                         k@@@@@@@@@
+echo @@@@@@@@Q-                         `j@@@@@@@@@@j`                         -Q@@@@@@@@
+echo @@@@@@@@T                        `}B@@@Qd3KdQ@@@B}`                        l@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@#:                       _D@@#V:      :V#@@E_                       :#@@@@@@@
+echo @@@@@@@g`                      `d@@Q:          :Q@@d`                      `g@@@@@@@
+echo @@@@@@@d                       ~@@@v            v@@@~                       d@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@v            v@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Q:          :Q@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#y:      :V#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Qd33dQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@0I0#@@@@#0I0@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@W'  `-,,-`  'W@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@k`            `k@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@L                L@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#r                  r#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B=                    =B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@g,                      ,g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@I`                        `X@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@}                            }@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#)                              )#@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@#r                                r#@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@#du^-                        -^uO#@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@QZk]*!_'`      `'_!*]kZQ@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BQQQQB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 1 /NOBREAK >nul
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+timeout /T 3 /NOBREAK >nul
+pause
+goto menu
+:brokenMode
+set CrashReason == 1
+cls
+echo =(
+timeout /T 2 /NOBREAK >nul
+echo %random%
+timeout /T 2 /NOBREAK >nul
+echo %random%%random%%random%%random%%random%%random%%random%
+timeout /T 4 /NOBREAK >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /T 3 /NOBREAK >nul
+echo DRIVE CORRUPT
+timeout /T 1 /NOBREAK >nul
+echo 0x010100000000 DOS error 
+timeout /T 5 /NOBREAK >nul
+echo please wait......
+timeout /T 7 /NOBREAK >nul
+echo repair cannot fix your device. Trying to reset system
+timeout /T 3 /NOBREAK >nul
+cls
+timeout /T 3 /NOBREAK >nul
+echo restarting....
+timeout /T 2 /NOBREAK >nul
+echo BOOTING INTO TRINITY RRBI
+timeout /T 5 /NOBREAK >nul
+pause
+function CRASH_HANDLER (
+	echo crash.0x01100100F001.%CrashReason%.INCL.%random% > crashHandles_%random%.%random%
+)
+set %CrashReason% == "ManuallyInitiatedCrash"
+CRASH_HANDLER
+echo Oh! we are taking you to the boot screen because you initiated a crash :) and we saved all your crash info to a file.
+timeout /T 2 /NOBREAK >nul
+goto boot
+:hi
+cls
+echo hi! Remember to do backups!
+pause
+goto menu
+:textColor
+cls
+echo 1.green 2.white 3.gray
+set txtColor= pick:
+if %txtColor% == 1 goto setGreen
+if %txtColor% == 2 goto setWhite
+if %txtColor% == 3 goto setGray
+:setGreen
+color 0a
+echo here you go!
+pause
+goto menu
+:setGray
+color 8
+echo here you go with your text color!
+pause
+goto menu
+:setWhite
+color F
+echo ready!
+pause
+goto menu
+:butterDeploy
+cls
+echo hello user! please enter your country code (eg. in)
+set loc= country:
+pause
+echo ok. We will do some important setup
+timeout /T 1 /NOBREAK >nul
+echo copying registers.....
+echo lisense: THIS SOFTWARE IS LICENSED UNDER GPL PUBLIC 3.0 (2007). > LICENSE.txt
+timeout /T 1 /NOBREAK >nul
+echo expanding registers.....
+echo .NewUser:True.Setup >> boot.INF
+timeout /T 1 /NOBREAK >nul
+echo completing setup.....
+echo .restart_setup >> boot.INF
+timeout /T 2 /NOBREAK >nul
+echo rebooting your computer
+cls
+echo congrats! Setup has completed. NOTE: ANY FILES FROM THE INSTALL SHOULD NOT BE DELETED FROM YOUR COMPUTER
+pause
+goto boot
